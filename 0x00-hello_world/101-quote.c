7738938c22cf10b7d273145c80a4d1b214ab9e42
Ibraheem Olagunju <olagunjuibraheem1@gmail.com>
2023-08-13 20:22:26 -0700

    A program that prints exactly and that piece of art is useful

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always (1)
+ */
+int main(void)
+{
+	char data[] =  "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	size_t elements_written = fwrite(data, sizeof(char), sizeof(data) - 1, stderr);
+
+	if (elements_written != sizeof(data) - 1)
+	{
+		perror("Error writing to stderr");
+
+		return (1);
+	}
+	return (1);
+}
