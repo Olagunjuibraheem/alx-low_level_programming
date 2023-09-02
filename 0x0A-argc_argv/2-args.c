64027f055d57ccd549e8c73c28aab6bc5242a695
Ibraheem Olagunju <olagunjuibraheem1@gmail.com>
2023-09-02 11:32:04 -0700

    A program that prints all arguments it receives

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
- * main - program that prints its name, followed by a new line.
- * @argc: Number of arguements 
- * @argv: Arrays of argument 
+ * main - program that prints all arguments it receives.
+ * @argc: Number of arguements
+ * @argv: Arrays of argument
  * Return: Always (0)
  */
 int main(int argc, char *argv[])
