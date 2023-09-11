27867efbf39f1e00bd32eb571f6994808e52f1cb
Ibraheem Olagunju <olagunjuibraheem1@gmail.com>
2023-09-10 23:40:47 -0700

    A program that adds positive numbers

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,34 +1,50 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
 /**
- * main - program that adds positive numbers.
- * @argc: Number of string
- * @argv: Array of string arrays
- * Return: (1) If program arguments contain letter, otherwise return (0).
+ * check_num - check - string there are digit
+ * @str: array str
+ * Return: Always 0 (Success)
+ */
+int check_num(char *str)
+{
+	unsigned int count;
+
+	count = 0;
+	while (count < strlen(str))
+	{
+		if (!isdigit(str[count]))
+			return (0);
+		count++;
+	}
+	return (1);
+}
+/**
+ * main - Print the name of the program
+ * @argc: Count arguments
+ * @argv: Arguments
+ * Return: Always 0 (Success)
  */
 int main(int argc, char *argv[])
 {
-	int i, sum;
+	int count, str_to_int, sum = 0;
 
-	if (argc == 1)
+	count = 1;
+	while (count < argc)
 	{
-		printf("0\n");
-		return (0);
-	}
-	if (argc > 1)
-	{
-		sum = 0;
-		for (i = 1; i < argc; i++)
+		if (check_num(argv[count]))
 		{
-			if (*argv[i] < 48 || *argv[i] > 57)
-			{
-				printf("Error\n");
-				return (1);
-			}
-			else
-				sum += atoi(argv[i]);
+			str_to_int = atoi(argv[count]);
+			sum += str_to_int;
 		}
-		printf("%d\n", sum);
+		else
+		{
+			printf("Error\n");
+			return (1);
+		}
+		count++;
 	}
+	printf("%d\n", sum); /*print sum*/
 	return (0);
 }
