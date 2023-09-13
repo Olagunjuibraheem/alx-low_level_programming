207374841b03bc00687719a898e5207daa577c0e
Ibraheem Olagunju <olagunjuibraheem1@gmail.com>
2023-09-13 15:23:07 -0700

    A function that concatenates two strings

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -3,15 +3,14 @@
 
 /**
  * string_nconcat - function that concatenates two strings.
- * @s1: string 1
- * @s2: string 2
- * @n:
+ * @s1: string to append
+ * @s2: string to concantenate
+ * @n: number of bytes from s2 to concantenate to s1
  * Return: NULL if it fail.
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	int i, l1, l;
-	unsigned int j, l2;
+	unsigned int i, j, l1, l2, l;
 	char *s3;
 
 	if (s1 == NULL)
@@ -23,29 +22,52 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	while (s1[l1] != '\0')
 		l1++;
 	l2 = 0;
-	while (l2 <= n)
+	while (s2[l2] != '\0')
+		l2++;
+	if (n >= l2)
 	{
-		if (s2[l2] != '\0')
-			l2++;
-	}
-	l = l1 + l2;
-	s3 = malloc(l + 1);
-	if (s3 == NULL)
-		return (NULL);
-	i = 0;
-	while (s1[i] != '\0')
-	{
-		s3[i] = s1[i];
-		j = 0;
-		while (j <= n)
+		l = l1 + l2;
+		s3 = malloc(l + 1);
+		if (s3 == NULL)
+			return (NULL);
+		i = 0;
+		while (s1[i] != '\0')
 		{
-			if (s2[j] != '\0')
+			s3[i] = s1[i];
+			j = 0;
+			while (j <= n)
 			{
-				s3[i] = s1[j];
+				if (s2[j] != '\0')
+				{
+					s3[i] = s1[j];
+				}
+				j++;
 			}
-			j++;
+			i++;
 		}
-		i++;
+		s3[i] = '\0';
+		return (s3);
+	}
+	else
+	{
+		s3 = malloc((l + 1) + n);
+		if (s3 == NULL)
+			return (NULL);
+		i = 0;
+		while (s1[i] != '\0')
+		{
+			s3[i] = s1[i];
+			j = 0;
+			while (j <= n)
+			{
+				if (s2[j] != '\0')
+				{
+					s3[i] = s1[j];
+				}
+				j++;
+			}
+			i++;
+		}
 	}
 	s3[i] = '\0';
 	return (s3);
