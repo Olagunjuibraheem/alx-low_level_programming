a5efac3d95eab26de6ebdce1d752e8c7f0a5c302
Ibraheem Olagunju <olagunjuibraheem1@gmail.com>
2023-09-14 14:09:47 -0700

    A function that concatenates two strings

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -10,29 +10,38 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *s;
-	unsigned int i = 0, j = 0, len1 = 0, len2 = 0;
+	unsigned int i, j, l1, l2, l;
+	char *s3;
 
-	while (s1 && s1[len1])
+	if (s1 == NULL)
+		s1 = "";
+	if (s1 == NULL)
+		s2 = "";
 
-		len1++;
-	while (s2 && s2[len2])
-		len2++;
-	if (n < len2)
-		s = malloc(sizeof(char) * (len1 + n + 1));
-	else
-		s = malloc(sizeof(char) * (len1 + len2 + 1));
-	if (!s)
-		return (NULL);
-	while (i < len1)
+	l1 = 0;
+	while (s1[l1] != '\0')
+		l1++;
+	l2 = 0;
+	while (s2[l2] != '\0')
+		l2++;
+	if (n >= l2)
 	{
-		s[i] = s1[i];
-		i++;
+		l = l1 + l2;
+		s3 = malloc(sizeof(char) * (l + 1));
 	}
-	while (n < len2 && i < (len1 + n))
-		s[i++] = s2[j++];
-	while (n >= len2 && i < (len1 + len2))
-		s[i++] = s2[j++];
-	s[i] = '\0';
-	return (s);
+	else
+		s3 = malloc(sizeof(char) * (l1 + n + 1));
+	if (s3 == NULL)
+		return (NULL);
+	i = 0;
+	while (s1[i] != s1[l1])
+		s3[i] = s1[i], i++;
+	i = l1;
+	j = 0;
+	while (n < l2 && i < l1 + n)
+		s3[i] = s2[j], i++, j++;
+	while (n >= l2 && i < l)
+		s3[i] = s2[j], i++, j++;
+	s3[i] = '\0';
+	return (s3);
 }
