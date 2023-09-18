12e4cd6b5dff73df3fc1ace4d1efe903a3f022ef
Ibraheem Olagunju <olagunjuibraheem1@gmail.com>
2023-09-17 17:18:27 -0700

    A #include <stdio.h>

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Short description
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Description: Longer description
+ */
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} dog;
+
+#endif /* DOG_H */
