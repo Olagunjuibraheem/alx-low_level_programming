b88dc217cf1d6f91b1b46afc076bb32ba41870e1
Olagunjuibraheem <olagunjuibraheem1@gmail.com>
2023-10-27 05:40:24 -0700

    This is a program that prints the number of arguments passed into it.

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - a program that prints the number of arguments passed into it.
+ * @argc: number/ size of command line arguments
+ * @argv: poinyer to array of command line arguments
+ *
+ * Return: Always (0).
+ */
+int main(int argc, char __attribute__((unused)) **argv)
+{
+	if (argc >= 1 )
+		printf("%d\n", argc);
+	
+	return (0);
+}
