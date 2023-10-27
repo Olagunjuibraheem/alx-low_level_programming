c4b082683823717adaefa521c3b2e2126312e3c3
Olagunjuibraheem <olagunjuibraheem1@gmail.com>
2023-10-27 02:40:54 -0700

    This is a program that prints its name, followed by a new line.

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - a program that prints its name, followed by a new line.
+ * @argc: number or size of commmand line arguments
+ * @argv: point€r to array of command line arguments
+ *
+ * Return: Always 0
+ */
+int main(int argc, char **argv)
+{
+	if (argc == 1)
+		printf("%s\n", argv[0]);
+
+	return(0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+This is a readme on the peoject: 0x0A. C - argc, argv
